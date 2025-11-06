5cf89a119d4144a443640b3e1ceb8323d4dcb1e1
yama <s24c1031tu@s.chibakoudai.jp>
2025-11-06 13:06:20 +0900

    Update test.bash and plus program

--- test.bash
+++ test.bash
@@ -1,24 +1,25 @@
-#!/bin/bash -xv
-# SPDX-fileCopyrightText: 2025 Yama0511
-# SPDX-License-Identefier: GPL-3.0-only
+ #!/bin/bash -xv
+ # SPDX-FileCopyrightText: 2025 Obata Yuuto
+ # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
- echo $1行目が違うよ
- res=1
-}
+ ng () {
+         echo ${1}行目が違うよ
+         res=1
+ }
 
-res=0
+ res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}"=15 ] || ng "$LINENO"
+ out=$(seq 5 | ./plus)
+ [ "${out}" = 15 ] || ng "$LINENO"
 
-out=$(echo あ| ./plus)
-[ "$?" =1 ]      || ng "$LINENO"
-[ "${out}"= "" ] || ng "$LINENO"
+ out=$(echo あ | ./plus)
+ [ "$?" = 1 ]      || ng "$LINENO"
+ [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" =1 ]      || ng "$LINENO"
-[ "${out}"= "" ] || ng "$LINENO"
 
-["$res" = 0 ] && echo OK
-exit $res
+ out=$(echo | ./plus)
+ [ "$?" = 1 ]      || ng "$LINENO"
+ [ "${out}" = "" ] || ng "$LINENO"
+
+ [ "$res" = 0 ] && echo OK
+ exit $res
